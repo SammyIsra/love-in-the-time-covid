27a6620a4e0fe05cb70894be9581bd74b3c0b237
Sammy Israwi <sammy.israwi@gmail.com>
2020-04-07 20:38:29 -0700

    Making things work?

--- lambda/covidCount.js
+++ lambda/covidCount.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 module.exports.handler = function(event, context) {
