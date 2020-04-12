79cf701b7fd92699d636cc2e0b4df88558701e05
Sammy Israwi <sammy.israwi@gmail.com>
2020-04-11 20:52:11 -0700

    Attempt to use local storage on SSR

--- src/utils/usePersistentStorage.ts
+++ src/utils/usePersistentStorage.ts
@@ -1,8 +1,23 @@
 import { useEffect, useState } from "react";
 
+/** Moched storage to be used when rendering on server side */
+const mockedStorage: Storage = {
+  length: 0,
+  clear() {},
+  getItem(key) {
+    return "";
+  },
+  key(index) {
+    return "";
+  },
+  removeItem(key) {},
+  setItem(key, value) {}
+};
+
 /**
  * Hook to use either localStorage or sessionStorage.
  * I got the idea from a talk by Michael Jackson about composability in React
+ * This was copied from a test page I made but modified to allow for SSR pages (aka to work with Gatsby)
  */
 function usePersistentState(
   storage: Storage,
@@ -30,7 +45,11 @@ function usePersistentState(
  * @param initialValue
  */
 export function useLocalStorageState(key: string, initialValue: string = "") {
-  return usePersistentState(localStorage, key, initialValue);
+  return usePersistentState(
+    typeof window !== "undefined" ? localStorage : mockedStorage,
+    key,
+    initialValue
+  );
 }
 /**
  * Hook to use either localStorage
@@ -38,5 +57,9 @@ export function useLocalStorageState(key: string, initialValue: string = "") {
  * @param initialValue
  */
 export function useSessionStorageState(key: string, initialValue: string = "") {
-  return usePersistentState(sessionStorage, key, initialValue);
+  return usePersistentState(
+    typeof window !== "undefined" ? window.sessionStorage : mockedStorage,
+    key,
+    initialValue
+  );
 }
