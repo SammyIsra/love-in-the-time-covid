c8810dd097a891e101b2880ba3a945f823267d8b
Sammy Israwi <sammy.israwi@gmail.com>
2020-04-16 10:14:11 -0700

    'About' links open in a new tab

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,13 +11,24 @@ const AboutPage: React.FC<PageProps> = props => {
         <FlexCenter>
           <h1>Want to lend a hand? ✋</h1>
           <p>
-            Hope this was marginally fun to scroll through! 🤣 For basics and other resources,{" "}
-            <a href="https://coronavirus.jhu.edu/#covid-19-basics">here</a> is a
-            good starting point. If you have any good news or personal stories
-            to share, feel free to{" "}
-            <a href="mailto:helenlyhuang@gmail.com">email</a> or{" "}
-            <a href="https://twitter.com/herenhuang">tweet</a> at me so it can
-            be included in the stack! Reach out for anything! 
+            Hope this was marginally fun to scroll through! 🤣 For basics and
+            other resources,{" "}
+            <a
+              href="https://coronavirus.jhu.edu/#covid-19-basics"
+              target="_blank"
+            >
+              here
+            </a>{" "}
+            is a good starting point. If you have any good news or personal
+            stories to share, feel free to{" "}
+            <a href="mailto:helenlyhuang@gmail.com" target="_blank">
+              email
+            </a>{" "}
+            or{" "}
+            <a href="https://twitter.com/herenhuang" target="_blank">
+              tweet
+            </a>{" "}
+            at me so it can be included in the stack! Reach out for anything!
           </p>
         </FlexCenter>
       </IndexPageLayout>
